29c672d4cb8e1bcf1c403ab01d1ed5f69f2ba6ec
Vitaliy Ukrainskiy <vitaliy.ukrainskiy@gameloft.com>
2024-09-28 23:31:53 +0300

    feat: update workflow

---
+++ iam.tf
@@ -0,0 +1,59 @@
+resource "aws_iam_role" "GithubActionsRole" {
+  name = "GithubActionsRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = "arn:aws:iam::${var.aws_account_id}:oidc-provider/token.actions.githubusercontent.com"
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          },
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo_name}:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "GithubActionsRole_ec2" {
+  role       = aws_iam_role.GithubActionsRole.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "GithubActionsRole_eventbridge" {
+  role       = aws_iam_role.GithubActionsRole.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "GithubActionsRole_route53" {
+  role       = aws_iam_role.GithubActionsRole.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "GithubActionsRole_s3" {
+  role       = aws_iam_role.GithubActionsRole.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "GithubActionsRole_sqs" {
+  role       = aws_iam_role.GithubActionsRole.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "GithubActionsRole_vpc" {
+  role       = aws_iam_role.GithubActionsRole.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "GithubActionsRole_iam" {
+  role       = aws_iam_role.GithubActionsRole.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
+}
--- s3.tf
+++ s3.tf
@@ -1,11 +1,3 @@
-provider "aws" {
-  region = "eu-west-1"
-}
-
 resource "aws_s3_bucket" "app_bucket" {
   bucket = "terraform-app-bucket-rsschool"
-}
-
-output "bucket_name" {
-  value = aws_s3_bucket.app_bucket.bucket
 }
