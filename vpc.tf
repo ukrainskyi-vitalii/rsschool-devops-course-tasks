896a540e16c0811e8b4378181f3387b780a97086
Vitaliy Ukrainskiy <vitaliy.ukrainskiy@gameloft.com>
2024-10-08 00:16:10 +0300

    feat: add subnets

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "rsschool_vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags                 = {
+  tags = {
     Name = "rsschool_vpc"
   }
 }
@@ -10,7 +10,7 @@ resource "aws_vpc" "rsschool_vpc" {
 # Internet Gateway for the public subnet
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.rsschool_vpc.id
-  tags   = {
+  tags = {
     Name = "rsschool_igw"
   }
 }
@@ -20,7 +20,7 @@ resource "aws_subnet" "public-subnet-1" {
   vpc_id            = aws_vpc.rsschool_vpc.id
   cidr_block        = var.public_subnet_1_cidr
   availability_zone = var.availability_zones[0]
-  tags              = {
+  tags = {
     Name = "public-terraform-rsschool-subnet-1"
   }
 }
@@ -29,7 +29,7 @@ resource "aws_subnet" "public-subnet-2" {
   vpc_id            = aws_vpc.rsschool_vpc.id
   cidr_block        = var.public_subnet_2_cidr
   availability_zone = var.availability_zones[1]
-  tags              = {
+  tags = {
     Name = "public-terraform-rsschool-subnet-2"
   }
 }
@@ -39,7 +39,7 @@ resource "aws_subnet" "private-subnet-1" {
   vpc_id            = aws_vpc.rsschool_vpc.id
   cidr_block        = var.private_subnet_1_cidr
   availability_zone = var.availability_zones[0]
-  tags              = {
+  tags = {
     Name = "private-terraform-rsschool-subnet-1"
   }
 }
@@ -48,7 +48,7 @@ resource "aws_subnet" "private-subnet-2" {
   vpc_id            = aws_vpc.rsschool_vpc.id
   cidr_block        = var.private_subnet_2_cidr
   availability_zone = var.availability_zones[1]
-  tags              = {
+  tags = {
     Name = "private-terraform-rsschool-subnet-2"
   }
 }
