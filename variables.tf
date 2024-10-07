7068a493a3fb4728f26c9e7e922fe8d36d2a03ee
Vitaliy Ukrainskiy <vitaliy.ukrainskiy@gameloft.com>
2024-10-07 23:47:50 +0300

    feat: add subnets

--- variables.tf
+++ variables.tf
@@ -14,3 +14,29 @@ variable "vpc_cidr" {
   description = "CIDR Block for VPC"
   default     = "10.0.0.0/16"
 }
+
+variable "public_subnet_1_cidr" {
+  description = "CIDR Block for Public Subnet 1"
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet_2_cidr" {
+  description = "CIDR Block for Public Subnet 2"
+  default     = "10.0.2.0/24"
+}
+
+variable "private_subnet_1_cidr" {
+  description = "CIDR Block for Private Subnet 1"
+  default     = "10.0.3.0/24"
+}
+
+variable "private_subnet_2_cidr" {
+  description = "CIDR Block for Public Subnet 2"
+  default     = "10.0.4.0/24"
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["eu-west-1a", "eu-west-2b", "eu-west-2c"]
+}
--- vpc.tf
+++ vpc.tf
@@ -7,9 +7,49 @@ resource "aws_vpc" "rsschool_vpc" {
   }
 }
 
+# Internet Gateway for the public subnet
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.rsschool_vpc.id
   tags = {
     Name = "rsschool_igw"
+  }
+}
+
+# Public subnets
+resource "aws_subnet" "public-subnet-1" {
+  vpc_id = aws_vpc.rsschool_vpc.id
+  cidr_block = var.public_subnet_1_cidr
+  availability_zone = var.availability_zones[0]
+  tags = {
+    Name = "public-terraform-rsschool-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public-subnet-2" {
+  vpc_id = aws_vpc.rsschool_vpc.id
+  cidr_block = var.public_subnet_2_cidr
+  availability_zone = var.availability_zones[1]
+  tags = {
+    Name = "public-terraform-rsschool-subnet-2"
+  }
+}
+
+
+# Private subnets
+resource "aws_subnet" "private-subnet-1" {
+  vpc_id = aws_vpc.rsschool_vpc.id
+  cidr_block = var.private_subnet_1_cidr
+  availability_zone = var.availability_zones[0]
+  tags = {
+    Name = "private-terraform-rsschool-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private-subnet-2" {
+  vpc_id = aws_vpc.rsschool_vpc.id
+  cidr_block = var.private_subnet_2_cidr
+  availability_zone = var.availability_zones[1]
+  tags = {
+    Name = "private-terraform-rsschool-subnet-2"
   }
 }
